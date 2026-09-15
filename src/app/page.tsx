c77224d9371efd1bc262e042aa06a6f1b8a3a7ec
Sorra White Night <sorra.whitenight@gmail.com>
2026-09-15 14:04:06 +0530

    feat: add full-width promotional banner with optimized WebP image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Menu from "@/components/Menu";
 import Gallery from "@/components/Gallery";
+import Banner from "@/components/Banner";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -15,6 +16,7 @@ export default function Home() {
         <About />
         <Menu />
         <Gallery />
+        <Banner />
         <Contact />
       </main>
       <Footer />
